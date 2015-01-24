3b90eca4b2546b6a4a043250fac6eaf860e41af0
ibebbs <ibebbs@bebbs.co.uk>
2015-01-24 08:46:30 +0000

    Added ninject bindings (with instrumentation)

--- src/OneCog.Io.Spak.Ninject/Module.cs
+++ src/OneCog.Io.Spak.Ninject/Module.cs
@@ -16,10 +16,10 @@ namespace OneCog.Io.Spak.Ninject
 
         public override void Load()
         {
-            //Bind<string>().ToConstant(_accessToken).WhenInjectedExactlyInto<RestClient>().InSingletonScope();
+            Bind<string>().ToConstant(_accessToken).WhenInjectedExactlyInto<RestClient>().InSingletonScope();
 
-            //Bind<RestClient>().ToSelf();
-            //Bind<IRestClient>().ToMethod(context => TracingProxy.CreateWithActivityScope<IRestClient>(context.Kernel.Get<RestClient>())).InSingletonScope();
+            Bind<RestClient>().ToSelf();
+            Bind<IRestClient>().ToMethod(context => TracingProxy.CreateWithActivityScope<IRestClient>(context.Kernel.Get<RestClient>())).InSingletonScope();
 
             Bind<Api>().ToSelf();
             Bind<IApi>().ToMethod(context => TracingProxy.CreateWithActivityScope<IApi>(context.Kernel.Get<Api>())).InSingletonScope();
