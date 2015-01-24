7e07df6b73d91f34e4568562846cdeba41c7ded3
ibebbs <ibebbs@bebbs.co.uk>
2015-01-24 09:21:29 +0000

    Added test to assert observable exception behavior

--- src/OneCog.Io.Spark.Test/ApiTestFixture.cs
+++ src/OneCog.Io.Spark.Test/ApiTestFixture.cs
@@ -3,6 +3,7 @@ using Microsoft.Reactive.Testing;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -56,6 +57,32 @@ namespace OneCog.Io.Spark.Test
 
             A.CallTo(() => _apiClient.Get(new Uri("https://api.spark.io/v1/devices/53ff6c065075535119511687/temperature"))).MustHaveHappened(Repeated.Exactly.Twice);
             Assert.That(actual.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ShouldPerpetuateVariableObservableException()
+        {
+            A.CallTo(() => _apiClient.Get(new Uri("https://api.spark.io/v1/devices/53ff6c065075535119511687/temperature"))).ReturnsLazily(call => Task.FromResult(Resources.JsonVariable.ToStream()));
+
+            TestScheduler scheduler = new TestScheduler();
+
+            List<IVariable> actual = new List<IVariable>();
+
+            IObservable<IVariable> observable = _api.ObserveVariable("53ff6c065075535119511687", "temperature", TimeSpan.FromSeconds(10), scheduler);
+
+            Exception exception = null;
+            observable.Subscribe(actual.Add, ex => exception = ex);
+
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(1).Ticks);
+
+            A.CallTo(() => _apiClient.Get(new Uri("https://api.spark.io/v1/devices/53ff6c065075535119511687/temperature"))).MustHaveHappened(Repeated.Exactly.Once);
+            Assert.That(actual.Count, Is.EqualTo(1));
+
+            A.CallTo(() => _apiClient.Get(new Uri("https://api.spark.io/v1/devices/53ff6c065075535119511687/temperature"))).ReturnsLazily(call => TaskEx.FromException<Stream>(new InvalidOperationException()));
+
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(9).Ticks);
+
+            Assert.That(exception, Is.Not.Null);
         }
     }
 }
---
+++ src/OneCog.Io.Spark.Test/TaskEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OneCog.Io.Spark.Test
+{
+    public static class TaskEx
+    {
+        public static Task<T> FromException<T>(Exception exception)
+        {
+            TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
+            tcs.SetException(exception);
+            return tcs.Task;
+        }
+    }
+}
