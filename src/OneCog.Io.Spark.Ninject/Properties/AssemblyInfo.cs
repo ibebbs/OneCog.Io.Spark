c4dde8ec97e423a68456a72ab0765a2893f40191
ibebbs <ibebbs@bebbs.co.uk>
2015-01-24 09:03:17 +0000

    Fixed up assembly info

--- src/OneCog.Io.Spark.Ninject/Properties/AssemblyInfo.cs
+++ src/OneCog.Io.Spark.Ninject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OneCog.Io.Spak.Ninject")]
+[assembly: AssemblyTitle("OneCog.Io.Spark.Ninject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OneCog.Io.Spak.Ninject")]
+[assembly: AssemblyProduct("OneCog.Io.Spark.Ninject")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
